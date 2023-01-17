071474affbb285dc69b4d166135c257f0acbfe1c
Soyendril <122810228+Soyendril@users.noreply.github.com>
2023-01-17 17:38:26 +0100

    Add files via upload

---
+++ JavaScript/ExercicesJavaScript.js
@@ -0,0 +1,13 @@
+===Declaration des Variables et Classes===
+
+class Book {
+	constructor (title, author, pages){
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+	}
+}
+
+let Booknumber1 = new Book("Code Civil", "Dalloz", 1580);
+let Booknumber2 = new Book("Code Penal", "Dalloz", 1794);
+let Booknumber3 = new Book("Code du travail", "Dalloz", 2150);
